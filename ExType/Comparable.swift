968a035ae571a58b945c9a0fa617a6f766cc2fce
daniel wang <wangyuanou@foxmail.com>
2019-01-10 16:31:30 +0800

    set limit

---
+++ ExType/Comparable.swift
@@ -0,0 +1,25 @@
+//
+//  Comparable.swift
+//  ExType
+//
+//  Created by 王渊鸥 on 2019/1/5.
+//  Copyright © 2019 WangYuanOu. All rights reserved.
+//
+
+import Foundation
+
+public extension Comparable {
+    public mutating func limit(_ min: Self, _ max: Self) {
+        if self < min { self = min }
+        if self > max { self = max }
+    }
+    
+    public func limited(_ min: Self, _ max: Self) -> Self {
+        if self < min { return min }
+        if self > max { return max }
+        return self
+    }
+}
+
+
+
