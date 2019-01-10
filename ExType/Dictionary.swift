330f3d586b5892e6b640a41e0b95663eaf3c108b
daniel wang <wangyuanou@foxmail.com>
2019-01-10 16:31:19 +0800

    move dictionary to collection

--- ExType/Dictionary.swift
+++ ExType/Dictionary.swift
@@ -9,29 +9,9 @@
 import Foundation
 
 public extension Dictionary {
-    public func map<K, V>(_ converter: (Key, Value) -> (K, V)?) -> [K: V] {
-        var result: [K: V] = [:]
-        for item in self {
-            if let convertedItem = converter(item.key, item.value) {
-                result[convertedItem.0] = convertedItem.1
-            }
-        }
-        return result
-    }
-
-    public var json: String? {
+    public var jsonString: String? {
         guard let jsonData = try? JSONSerialization.data(withJSONObject: self, options: []) else { return nil }
         let jsonString = String(data: jsonData, encoding: String.Encoding.utf8)
         return jsonString
     }
-}
-
-public func ex_json(text: String) -> [String: Any]? {
-    if let data = text.data(using: String.Encoding.utf8) {
-        if let dict = try? JSONSerialization.jsonObject(with: data, options: []) {
-            return dict as? [String: Any]
-        }
-    }
-
-    return nil
 }
