897a60dd56b12a80c1b7df5aa4bee65f56a1cffb
daniel wang <wangyuanou@foxmail.com>
2019-01-11 11:57:23 +0800

    add ~ operator for CG***

--- ExType/Float.swift
+++ ExType/Float.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreGraphics
 
 public func % (_ lhs:Double, _ rhs:Double) -> Double {
     return lhs.remainder(dividingBy: rhs)
@@ -26,4 +27,21 @@ public func ~(_ lhs:Float, _ rhs:Float) -> Bool {
 public func ~(_ lhs:Double, _ rhs:Double) -> Bool {
     let distance = lhs - rhs
     return distance < 0.00001 && distance > -0.00001
+}
+
+public func ~(_ lhs:CGFloat, _ rhs:CGFloat) -> Bool {
+    let distance = lhs - rhs
+    return distance < 0.00001 && distance > -0.00001
+}
+
+public func ~(_ lhs:CGPoint, _ rhs:CGPoint) -> Bool {
+    return (lhs.x ~ rhs.x) && (lhs.y ~ rhs.y)
+}
+
+public func ~(_ lhs:CGSize, _ rhs:CGSize) -> Bool {
+    return (lhs.width ~ rhs.width) && (lhs.height ~ rhs.height)
+}
+
+public func ~(_ lhs:CGRect, _ rhs:CGRect) -> Bool {
+    return (lhs.origin ~ rhs.origin) && (lhs.size ~ rhs.size)
 }
